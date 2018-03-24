23bfc3e753819e83d8177b8e4f93c6a5fe890b2d
Enpi23 <enpi23.juang@Gmail.com>
2018-03-25 01:42:43 +0700

    Commit 0 Modul 5

---
+++ app/src/main/java/enpi23/modul5/prak/andro/victor_1202150100_modul5/MainActivity.java
@@ -0,0 +1,13 @@
+package enpi23.modul5.prak.andro.victor_1202150100_modul5;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/enpi23/modul5/prak/andro/victor_1202150100_modul5/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package enpi23.modul5.prak.andro.victor_1202150100_modul5;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
